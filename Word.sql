95cced0e6b4c493dc7d0d10fcc404c99b2c9aa5d
Sivenati <61004319+Sivenati@users.noreply.github.com>
2020-02-19 14:16:43 +0200

    Add files via upload

--- Word.sql
+++ Word.sql
@@ -1,13 +1,13 @@
-Chips,Quantity,SellingP,CostP,Size,Barcode,Discription,Quantity,SellingP,CostP,Size,Barcode,Discription
-Simba,24,15,13,200g,1234343,"Cheese  & onion",24,15,13,200g,1234343,"Cheese  & onion"
-Doritos,24,15,12,250,1234343,"Beef flavor",24,15,12,250,1234343,"Beef flavor"
-Lays,24,16,14,200g,1234343,"Cilli beef",24,16,14,200g,1234343,"Cilli beef"
-Pringles,24,18,14,200g,1234343,"Cheese  & onion",24,18,14,200g,1234343,"Cheese  & onion"
-Stylos,24,10,7,230g,1234343,Barbaque,24,10,7,230g,1234343,Barbaque
-"Craker Snacks",24,2,1,300g,1234343,"Beef flavor",24,2,1,300g,1234343,"Beef flavor"
-Gooslows,24,2,1,200g,1234343,tomato,24,2,1,200g,1234343,tomato
-Tomatoes,24,10,5,200g,1234343,"in bunch",24,10,5,200g,1234343,"in bunch"
-Potatoes,24,10,5,350g,1234343,"in bunch",24,10,5,350g,1234343,"in bunch"
-Cabbage,24,10,5,430g,1234343,"in bunch",24,10,5,430g,1234343,"in bunch"
-Carrot,24,10,5,500g,1234343,"in bunch",24,10,5,500g,1234343,"in bunch"
-Spinach,24,10,5,500g,1234343,"in bunch",24,10,5,500g,1234343,"in bunch"
+Chips,Quantity,SellingP,CostP,Size,Barcode,Discription
+Simba,24,15,13,200g,1234343,"Cheese  & onion"
+Doritos,24,15,12,250,1234343,"Beef flavor"
+Lays,24,16,14,200g,1234343,"Cilli beef"
+Pringles,24,18,14,200g,1234343,"Cheese  & onion"
+Stylos,24,10,7,230g,1234343,Barbaque
+"Craker Snacks",24,2,1,300g,1234343,"Beef flavor"
+Gooslows,24,2,1,200g,1234343,tomato
+Tomatoes,24,10,5,200g,1234343,"in bunch"
+Potatoes,24,10,5,350g,1234343,"in bunch"
+Cabbage,24,10,5,430g,1234343,"in bunch"
+Carrot,24,10,5,500g,1234343,"in bunch"
+Spinach,24,10,5,500g,1234343,"in bunch"
