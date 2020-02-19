b17184fb950b332da6d4dcb9fc746815f1ef4882
Sivenati <61004319+Sivenati@users.noreply.github.com>
2020-02-19 15:27:31 +0200

    Add files via upload

---
+++ NewSprint.sql
@@ -0,0 +1,19 @@
+Chips,Quantity,SellingP,CostP,Size,Barcode,Discription,Sweets,beers,Cigarets
+Simba,24,15,13,200g,1234343,"Cheese  & onion",NULL,NULL,NULL
+Doritos,24,15,12,250,1234343,"Beef flavor",NULL,NULL,NULL
+Lays,24,16,14,200g,1234343,"Cilli beef",NULL,NULL,NULL
+Pringles,24,18,14,200g,1234343,"Cheese  & onion",NULL,NULL,NULL
+Stylos,24,10,7,230g,1234343,Barbaque,NULL,NULL,NULL
+"Craker Snacks",24,2,1,300g,1234343,"Beef flavor",NULL,NULL,NULL
+Gooslows,24,2,1,200g,1234343,tomato,NULL,NULL,NULL
+Tomatoes,24,10,5,200g,1234343,"in bunch",NULL,NULL,NULL
+Potatoes,24,10,5,350g,1234343,"in bunch",NULL,NULL,NULL
+Cabbage,24,10,5,430g,1234343,"in bunch",NULL,NULL,NULL
+Carrot,24,10,5,500g,1234343,"in bunch",NULL,NULL,NULL
+Spinach,24,10,5,500g,1234343,"in bunch",NULL,NULL,NULL
+Doritos,24,11,23,200,1222233,"Cheese & onion",NULL,NULL,NULL
+Lays,55,45,51,200,1222233,"Cheese & onion",NULL,NULL,NULL
+Spookies,51,15,15,200,1222233,"Cheese & onion",NULL,NULL,NULL
+Doritos,24,11,23,200,1222233,"Cheese & onion",NULL,NULL,NULL
+Lays,55,45,51,200,1222233,"Cheese & onion",NULL,NULL,NULL
+Spookies,51,15,15,200,1222233,"Cheese & onion",NULL,NULL,NULL
